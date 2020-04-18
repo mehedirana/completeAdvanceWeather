4735ef3f1cff10ffbc53db4a9c80931b7408baff
mehedi rana <mdmehedirana@gmail.com>
2020-04-18 08:48:39 +0600

    error solve

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView  } from 'react-native';
 import { TextInput, Card, List} from 'react-native-paper';
 import Header from './components/Header'
 
@@ -10,50 +10,60 @@ class App extends React.Component {
     city: []
   }
 
-  fetchCity=(text)=>{
+  fetchCity = (text) => {
 
-    this.setState({text})
+    this.setState({ text })
     fetch(`http://autocomplete.wunderground.com/aq?query=${text}`)
-    .then(data=>{
-      return data.json();
-    })
-    .then(data2=>{
-     
-      this.setState({
-        city:data2.RESULTS.slice(0,9)
+      .then(data => data.json())
+      .then(data2 => {
+        this.setState({
+          city: data2.RESULTS.slice(0, 9)
+        })
       })
-    })
-    console.log(this.state.city)
+
   }
   render() {
-    cityList = <Card><List.Item title="no city"/></Card>
-    if(this.state.city.length>0){
-      cityList = this.state.city.map(cities=>{
-      return (
-        <Card><List.Item title={cities.name}/></Card>
-      )
-    })
+
+    //console.log(this.state.city)
+    cityList = <Card><List.Item title="no city" /></Card>
+
+    if (this.state.city.length > 0) {
+
+      cityList = this.state.city.map(cities => {
+        return (
+          <Card style={{ margin: 5 }} key={cities.lat}>
+            <List.Item title={cities.name} />
+          </Card>
+        )
+      })
+    }
+
     return (
       <View style={styles.container}>
-        <Header/>
+        <Header />
+
         <TextInput
-          label='Email'
+          label='City'
           value={this.state.text}
           onChangeText={text => this.fetchCity(text)}
         />
-        <Text>Open up App.js to start working on your app!</Text>
+
+        <ScrollView>
+          {cityList}
+        </ScrollView>
+
       </View>
     );
   }
 
 }
-}
+
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     // alignItems: 'center',
     // justifyContent: 'center',
   },
