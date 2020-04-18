e1c20aef06318f1c80510a65ff9923b7c5f6e496
mehedi rana <mdmehedirana@gmail.com>
2020-04-18 07:55:37 +0600

    add fetch

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Card, List} from 'react-native-paper';
 import Header from './components/Header'
 
 class App extends React.Component {
-  render() {
 
+  state = {
+    text: '',
+    city: []
+  }
+
+  fetchCity=(text)=>{
+
+    this.setState({text})
+    fetch(`http://autocomplete.wunderground.com/aq?query=${text}`)
+    .then(data=>{
+      return data.json();
+    })
+    .then(data2=>{
+     
+      this.setState({
+        city:data2.RESULTS.slice(0,9)
+      })
+    })
+    console.log(this.state.city)
+  }
+  render() {
+    cityList = <Card><List.Item title="no city"/></Card>
+    if(this.state.city.length>0){
+      cityList = this.state.city.map(cities=>{
+      return (
+        <Card><List.Item title={cities.name}/></Card>
+      )
+    })
     return (
       <View style={styles.container}>
         <Header/>
+        <TextInput
+          label='Email'
+          value={this.state.text}
+          onChangeText={text => this.fetchCity(text)}
+        />
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
 
 }
-
+}
 export default App;
 
 const styles = StyleSheet.create({
