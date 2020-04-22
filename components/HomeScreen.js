c0854c44499bb15037d0506b20c991facf31ee31
mehedi rana <mdmehedirana@gmail.com>
2020-04-22 11:40:56 +0600

    add api

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -3,6 +3,20 @@ import { StyleSheet, Text, View, ScrollView  } from 'react-native';
 import Header from './Header';
 
 class HomeScreen extends React.Component {
+  //api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
+  //key d7b1023e1d8d23485e603f31d14d9157
+
+  getWeather=()=>{
+
+    cityName = "dhaka";
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=d7b1023e1d8d23485e603f31d14d9157`)
+    .then(res=>{
+      return res.json()
+    })
+    .then(data=>{
+      console.log(data)
+    })
+  }
 
   render() {
 
