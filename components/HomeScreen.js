24bbace3bd80abff2cbbadffcf83914e484050a6
mehedi rana <mdmehedirana@gmail.com>
2020-04-22 12:50:19 +0600

    add data into info

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView  } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Header from './Header';
 
 class HomeScreen extends React.Component {
-  //api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
-  //key d7b1023e1d8d23485e603f31d14d9157
 
-  getWeather=()=>{
+  state = {
+    info: {
+      name: 'loading',
+      temp: 'loading',
+      humidity: 'loading',
+      description: 'loading',
+      icon: 'loading'
+    }
+  }
+
+  getWeather = () => {
 
     cityName = "dhaka";
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=d7b1023e1d8d23485e603f31d14d9157`)
-    .then(res=>{
-      return res.json()
-    })
-    .then(data=>{
-      console.log(data)
-    })
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        console.log(data)
+        this.setState({
+          info:
+          {
+            name: data.name,
+            temp: data.main.temp,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            icon: data.weather[0].icon
+          }
+        })
+        console.log("here data:")
+        console.log(this.state)
+      })
+      
+  }
+
+  componentDidMount() {
+    this.getWeather()
   }
 
   render() {
